13cec5db6a51459f9d3a2a431c5549eb165976a4
Scott Byrns <scott@scotts-MacBook-Pro.local>
2013-11-17 07:18:43 -0700

    Support for custom socket variable name in global namespace.

--- README.md
+++ README.md
@@ -16,3 +16,21 @@ Live Widget for connection to socket.io
 
 
 ```
+
+
+
+A custom gobal variable name can be specified when creating a socket. This way you can use more than one instance of this widget.
+
+```html
+
+
+<dataset data-widget="socket-io-connector"
+         data-host="https://localhost"
+         data-port="3000"
+         data-secure="true"
+		 data-namespace="mySocket">
+         
+</dataset>
+
+
+```
--- src/socket-io-connector.js
+++ src/socket-io-connector.js
@@ -4,16 +4,18 @@ LiveWidgets.addWidget({
 	controller: {},
 	constructor: function () {
 		
-        window.socket = io.connect(this.model.host, {
+		var socket = this.model.namespace || "socket";
+		
+        window[socket] = io.connect(this.model.host, {
             secure: this.model.secure,
             port: this.model.port
         });
 	
-        window.socket.on('error', function () {
+        window[socket].on('error', function () {
             console.error(arguments)
         });
 	
-        window.socket.on('message', function () {
+        window[socket].on('message', function () {
             console.log(arguments)
         });
 		
